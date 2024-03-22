468e074e6b833381cba8dac3c0d8b12ff9b1747d
hectormorenosoftware <hectormorenosoftware@gmail.com>
2024-03-22 01:33:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+This repo allows you to setup a cloud service using your laptop or desktop(Mac or PC) you can replicate AWS S3 to host images and Mongo DB to store data and images.
+
+Website ideas that you can make with this software:
+Youtube, Instagram, Facebook, Twitter, Google, AWS Buckets and Hosting Services, Microsoft Azure, Heroku, Netlify, Pinterest, Amazon, Netflix, Hulu, Disney +, Blockbuster, RedBox Online, Spotify, Apple Music, Apple TV, Direct TV, Sling TV, Toys R US, HBO Max, Yahoo Email, Hotmail, and Gmail.
+
+This an example on how to CRUD images and json files from Disk(your own SSD), this will replicate the behavior hosting services like AWS, Google Cloud, or Microsoft Cloud offer, to see an image go to https://localhost:5000/images/${nameofimage}${ext}.
+
+To install dependencies
+npm install
+
+To run application
+node index.js
+
+or
+
+nodemon index.js
+
+To serve it to the internet use https://ngrok.com/, you will have to have your localhost server on, ngrok will create a tunnel and will give you a url for people to see it in the internet, you can pay ngrok $20 per month to have a custom url.
+
+For example(your localhost host server will have to be running)
+
+You will have to download ngrok here:
+
+https://dashboard.ngrok.com/get-started/setup
+
+node index.js
+
+./ngrok http 5000 --host-header=rewrite
+
+Forwarding: https://f461-35-134-97-254.ngrok.io -> http://localhost:5000
+
+To see image:
+https://f461-35-134-97-254.ngrok.io/images/alexandra.jpeg
+
+To see video:
+http://localhost:5000/videos/avengersbattle.mp4
+
+To hear song:
+http://localhost:5000/songs/oceans14.mp3
+
+or
+
+http://localhost:5000/songs/mixvol2.mp3
+
+or
+
+http://localhost:5000/songs/jacksparrow.mp3
+
+To send emails to customers with https://sendgrid.com/
+
+1. You will need to create an api_key here https://app.sendgrid.com/settings/api_keys
+2. You will need to verify Single Sender Verification https://app.sendgrid.com/settings/sender_auth
+
+Merge Videos like a cable station
+https://www.adobe.com/express/feature/video/merge?ef_id=CjwKCAiA7dKMBhBCEiwAO_crFKWqZzFylo8KcdzNbYcqKS81AyZ7uRlbWDbtwnoBtgpmXOTJD1LH7BoC4_4QAvD_BwE:G:s&s_kwcid=AL!3085!3!453145736114!!!g!!!1523460659!53763243570&gclid=CjwKCAiA7dKMBhBCEiwAO_crFKWqZzFylo8KcdzNbYcqKS81AyZ7uRlbWDbtwnoBtgpmXOTJD1LH7BoC4_4QAvD_BwE
+
+https://support.apple.com/guide/quicktime-player/record-your-screen-qtp97b08e666/mac
+
+https://support.apple.com/guide/quicktime-player/record-audio-qtpf25d6f827/mac
+
+https://cloudconvert.com/mov-to-mp4
+
+m4a to mp3:
+
+https://cloudconvert.com/m4a-converter
+
+https://www.amazon.com/External-CD-DVD-Drive-Compatible/dp/B07MJW5BXZ/ref=sr_1_4?keywords=disc+burner+amazon&qid=1637182126&sr=8-4
+
+https://www.amazon.com/dp/B07DLRG9VH/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B07DLRG9VH&pd_rd_w=UpgwR&pf_rd_p=54ed5474-54a8-4c7f-a88a-45f748d18166&pd_rd_wg=gRtLY&pf_rd_r=AM375S9DQJGJPEQSRWJ5&pd_rd_r=605bd036-4802-45e6-ac97-ed9e0ddf5a94&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSDFOWERSWDQ2UEcwJmVuY3J5cHRlZElkPUEwMDczMDM2MzhHRFc5STlCMEFKSCZlbmNyeXB0ZWRBZElkPUEwOTgwNjA5M04yQTZNWUVCV0VGJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==
---
+++ index.js
@@ -0,0 +1,225 @@
+const express = require("express");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
+const stripe = require("stripe")("");
+const transporter = nodemailer.createTransport(
+  sendGridTransport({
+    auth: {
+      api_key: "",
+    },
+  })
+);
+const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./build/images");
+  },
+  filename: function (req, file, cb) {
+    const fileName = path.parse(file.originalname).ext;
+
+    cb(null, req.params.userName + req.params.picNumber + fileName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+//The five at the end represents megabytes.
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+
+app.use(express.static("build"));
+
+app.use(cors());
+app.use(bodyParser.json());
+const YOUR_DOMAIN = "http://localhost:5000";
+
+app.post("/send-email", async (req, res) => {
+  try {
+    await transporter.sendMail({
+      to: "hm@gmail.com",
+      from: "hm@gmail.com",
+      subject: "Signup Succeeded",
+      html: "<h1>Hello world</h1>",
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
+});
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price: "price_1JtaTVJYfPxdOrXDLUMksGtI",
+        quantity: 1,
+      },
+    ],
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: "https://dashboardsapp.herokuapp.com/#/",
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.post("/charge-card", async (req, res) => {
+  const { payment_number, exp_month, exp_year, cvc, email } = req.body.params;
+
+  try {
+    const token = await stripe.tokens.create({
+      card: {
+        number: payment_number,
+        exp_month,
+        exp_year,
+        cvc,
+      },
+    });
+
+    const charge = await stripe.charges.create({
+      amount: 4000,
+      currency: "usd",
+      source: token.id,
+      description: "Dashboards monthly pay as you go subscription.",
+      receipt_email: email,
+    });
+
+    res.send(charge);
+  } catch (e) {
+    res.status(400).send("Credit Card Declined");
+    throw new Error(e);
+  }
+});
+
+app.get("/get-users", (req, res) => {
+  const rawdata = fs.readFileSync("./database/users.json");
+  const jsondata = JSON.parse(rawdata);
+
+  res.send(jsondata);
+});
+
+app.post("/create-route/:routeName", (req, res) => {
+  const rawdata = fs.readFileSync("./database/routes.json");
+  const jsonData = JSON.parse(rawdata);
+  const { routeName } = req.params;
+  jsonData["routes"].push(routeName);
+  const jsonToWrite = JSON.stringify(jsonData);
+  fs.writeFileSync("./database/routes.json", jsonToWrite);
+  res.send("It's alive!");
+});
+
+app.post("/create-user/:email/:username/:lastFourCard", (req, res) => {
+  const rawdata = fs.readFileSync("./database/users.json");
+  const jsondata = JSON.parse(rawdata);
+  const today = new Date();
+  const thirtyDaySubscription = new Date(today);
+  thirtyDaySubscription.setDate(thirtyDaySubscription.getDate() + 30);
+  thirtyDaySubscription.toLocaleDateString();
+
+  const { email, username, lastFourCard } = req.params;
+  jsondata[username] = {
+    email,
+    username,
+    lastFourCard,
+    paidSubscription: "Y",
+    published: "N",
+    dateOfPurchase: today.toLocaleDateString(),
+    dateOfExpiration: thirtyDaySubscription.toLocaleDateString(),
+    userProducts: [
+      {
+        image: "",
+        imageName: "",
+        titleOfProduct: "",
+        priceOfProduct: "",
+      },
+      {
+        image: "",
+        imageName: "",
+        titleOfProduct: "",
+        priceOfProduct: "",
+      },
+      {
+        image: "",
+        imageName: "",
+        titleOfProduct: "",
+        priceOfProduct: "",
+      },
+      {
+        image: "",
+        imageName: "",
+        titleOfProduct: "",
+        priceOfProduct: "",
+      },
+      {
+        image: "",
+        imageName: "",
+        titleOfProduct: "",
+        priceOfProduct: "",
+      },
+      {
+        image: "",
+        imageName: "",
+        titleOfProduct: "",
+        priceOfProduct: "",
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(jsondata);
+  fs.writeFileSync("./database/users.json", jsonData);
+  res.send("Success!");
+});
+
+app.post("/upload/:userName/:picNumber", upload.single("file"), (req, res) => {
+  if (req.file) {
+    res.json(req.file);
+  } else throw "error";
+});
+
+app.delete("/upload/:userName/:picNumber/:ext", (req, res) => {
+  const fileName =
+    req.params.userName + req.params.picNumber + "." + req.params.ext;
+
+  try {
+    fs.unlinkSync(`build/images/${fileName}`);
+
+    res.status(201).send({ message: "Image deleted" });
+  } catch (e) {
+    res.status(400).send({
+      message: "Error deleting image!",
+      error: e.toString(),
+      req: req.body,
+    });
+  }
+});
+
+//This creates a worker for every CPU, this allows the server to be ranned in a multi-thread manner, meaning you can use every
+//core in your machine to serve your application, this will make your server faster.
+
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    // Create a worker
+    cluster.fork();
+  }
+} else {
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("listenting");
+  });
+}
